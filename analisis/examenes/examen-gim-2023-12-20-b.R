6674ce78ec32adef722eff09d5539dee19bb2fda
Alfredo Sánchez Alberca <asalber@gmail.com>
2024-01-01 12:46:00 +0100

    Añadidos exámenes de análisis III.

---
+++ analisis/examenes/examen-gim-2023-12-20-b.R
@@ -0,0 +1,40 @@
+library("knitr")
+library("exams")
+
+setwd("analisis/examenes")
+
+degree <- "gim"
+date <- "2023-12-22"
+questions <- c("extcon-01-eco.Rmd", "tay2-01-gen.Rmd", "intn-05-gen.Rmd", "intn-04-gen.Rmd")
+
+exams2pdf(questions,
+  n=1,
+  name = c(paste0("examen-", degree, "-", date, "-b"), paste0("solucion-", degree, "-", date, "-b")),
+  encoding = "UTF-8", 
+  dir = "pdf",
+  edir = "../preguntas",
+  template = c("plantillas/examenceu.tex", "plantillas/solucionceu.tex"),
+  header = list(
+    Title = "EXAMEN DE ANÁLISIS",
+    Subject = "ANÁLISIS III",
+    Degree= "2º Grado en Ingeniería Matemática",
+    Date = date,
+    Version = "A",
+    Time = "1 hora y 15 minutos"
+  )
+)
+
+# Borrar directorio de imágenes del examen
+unlink("img/exam1", recursive = TRUE) 
+exams2pandoc(questions, n=1, 
+             name = paste0("examen-", degree, "-", date, "-b"), 
+             encoding = "UTF-8", 
+             type="latex",
+             dir = "latex",
+             edir = "../preguntas",
+             sdir = "img",
+             template = c("templates/plain.tex"),
+             svg=T
+)
+
+pandoc(paste0("latex/examen-", degree, "-", date, "-b1.latex"), format="markdown")
